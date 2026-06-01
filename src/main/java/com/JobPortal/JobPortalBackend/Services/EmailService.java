76cdc777d1eb24bd31c0ccd5b470884ce4d89092
Niwas Bondar <niwasbondar07@gmail.com>
2026-06-02 00:39:00 +0530

    logges added to email service

--- src/main/java/com/JobPortal/JobPortalBackend/Services/EmailService.java
+++ src/main/java/com/JobPortal/JobPortalBackend/Services/EmailService.java
@@ -1,5 +1,7 @@
 package com.JobPortal.JobPortalBackend.Services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,8 +12,10 @@ public class EmailService {
 
     @Autowired
     JavaMailSender javaMailSender;
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
     public void sendEmail(String to,String subject, String body){
+        logger.info("Attempting to send email to: {} with subject: {}", to, subject);
         try{
             SimpleMailMessage mail=new SimpleMailMessage();
             mail.setTo(to);
@@ -20,7 +24,10 @@ public class EmailService {
 
             javaMailSender.send(mail);
 
+            logger.info("Email sent successfully to: {}", to);
+
         } catch (Exception e) {
+            logger.error("Failed to send email to: {}. Error: {}", to, e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
